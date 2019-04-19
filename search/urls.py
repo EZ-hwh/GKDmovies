f080ca6136a002aeff5b773caff9cbfa5c5bbf01
EZ-hwh <240836592@qq.com>
2019-04-19 23:31:08 +0800

    nothing

--- search/urls.py
+++ search/urls.py
@@ -6,6 +6,8 @@ app_name='search'
 urlpatterns = [
     path('', views.Frontpage, name='frontpage'),
     path('search/', views.search, name='search'),
-    path('director/', views.Director, name='detail'),
+	path('actor/', views.actor, name='actor'),
+	path('movie/', views.movie, name='movie'),
+    path('director/', views.director, name='director'),
     path('<int:question_id>/user/', views.User, name='results'),
 ]
--- search/views.py
+++ search/views.py
@@ -9,21 +9,35 @@ def Frontpage(request):
 
 def search(request):
     q = request.GET.get('index')
+    p = request.GET.get('person')
 
     error_msg = ''
 
-    if not q:
+    if not q or not p:
         error_msg = '请输入关键词'
         return render(request, 'errors.html', {'error_msg': error_msg})
 
-    post_list = Actor.objects.filter(actor_name__icontains=q)
-    return render(request, 'results.html', {'error_msg': error_msg,
-                                               'post_list': post_list})
+    if p == 'actor' :
+        post_list = Actor.objects.filter(actor_name__contains=q)
+        return render(request, 'search_actor.html', {'error_msg': error_msg, 'post_list': post_list})
+    
+    if p == 'movie' :
+        post_list = Movie.objects.filter(movie_name__contains=q)
+        return render(request, 'search_movie.html', {'error_msg': error_msg, 'post_list': post_list})
+
+    if p == 'director' :
+        post_list = Director.objects.filter(director_name__contains=q)
+        return render(request, 'search_director.html', {'error_msg': error_msg, 'post_list': post_list})	
 
 def actor(request):
+    return render(request, 'actor.html', {})
     #latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #context = {'latest_question_list': latest_question_list}
-    return render(request, 'actor.html', {})
+	
+def movie(request):
+	return render(request, 'movie.html', {})
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #context = {'latest_question_list': latest_question_list}
     
 def director(request, question_id):
     return render(request, 'director.html', {})
