1bd460874f3adba87139b8d471568d97da296220
EZ-hwh <240836592@qq.com>
2019-04-19 17:05:46 +0800

    added actor wch

--- search/urls.py
+++ search/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
+import re
 
 app_name='search'
 urlpatterns = [
     path('', views.Frontpage, name='frontpage'),
-    # ex: /polls/
-    path('actor/', views.Actor, name='index'),
-    # ex: /polls/5/
+    path(r'^search/$', views.search, name='search'),
     path('director/', views.Director, name='detail'),
-    # ex: /polls/5/results/
     path('<int:question_id>/user/', views.User, name='results'),
 ]
--- search/views.py
+++ search/views.py
@@ -7,6 +7,19 @@ from django.shortcuts import render,get_object_or_404
 def Frontpage(request):
     return render(request, "Frontpage.html",{})
 
+def search(request):
+    q = request.GET.get('q')
+
+    error_msg = ''
+
+    if not q:
+        error_msg = '请输入关键词'
+        return render(request, 'errors.html', {'error_msg': error_msg})
+
+    post_list = Actor.objects.filter(actor_name__icontains=q)
+    return render(request, 'results.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
+
 def actor(request):
     #latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #context = {'latest_question_list': latest_question_list}
@@ -20,6 +33,3 @@ def director(request, question_id):
 def user(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
-
-#def vote(request, question_id):
-#    return HttpResponse("You're voting on question %s." % question_id)
