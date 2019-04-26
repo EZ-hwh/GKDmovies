392d89fee05d3cd95ea3bb489e12d6896029bc96
EZ-hwh <240836592@qq.com>
2019-04-26 16:10:42 +0800

    finished

--- search/admin.py
+++ search/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+
+admin.site.register(Person)
+admin.site.register(Movie)
+admin.site.register(User)
+admin.site.register(Play)
+admin.site.register(Direct)
+admin.site.register(Comment)
